1175fb1214661b558edb0ac2ffaeddfc4a0ee821
Navid Bin Ahmed <navid_bin_ahmed@yahoo.com>
2025-04-03 13:04:29 +0600

    created render db and updated files

--- app/database.py
+++ app/database.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+import os
 
 '''I used local PGAdmin 4 as a database that works on PostgreSQL.
 This simple file set ups the database and corresponding data sessions.'''
 
-DATABASE_URL = "postgresql://postgres:pga4_pass12@localhost:5432/data_pipeline"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
