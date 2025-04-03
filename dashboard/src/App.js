b56e910fa71e790db01aba2c2e99386a1c2b0d56
Navid Bin Ahmed <navid_bin_ahmed@yahoo.com>
2025-04-03 13:45:13 +0600

    updated yaml render file

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Table } from "antd";
 import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from "recharts";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
+const API_URL = "https://fastapi-f3s0.onrender.com/";
 console.log(API_URL);
 
 function App() {
